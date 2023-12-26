32a1cc1e3321ad326b87ed01d46f09b4e8aa6c5d
JuanAntBuit <juanantoniobuit@gmail.com>
2023-12-26 19:00:35 +0100

    Create MainActivityKtTest

---
+++ app/src/test/java/com/eightnoa/rulethree/ui/main/MainActivityKtTest.kt
@@ -0,0 +1,24 @@
+package com.eightnoa.rulethree.ui.main
+
+
+import org.junit.Before
+import org.junit.Test
+
+class MainActivityKtTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun title() {
+        //Given
+
+        //When
+
+
+        //Then
+        //TODO
+        assert(2 == 2)
+    }
+}
